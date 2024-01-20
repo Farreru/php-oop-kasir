104bb0cb763dd7bfcdd77428e1cf0823a9ba37a9
Farreru <m.adit.farrel@gmail.com>
2024-01-20 13:02:26 +0700

    inisiasi untuk dev

--- layout/footer.php
+++ layout/footer.php
@@ -24,7 +24,7 @@
 
 <!-- Template Main JS File -->
 <script src="<?= assets('js/main.js') ?>"></script>
-<script src="<?= assets('js/demo.js') ?>"></script>
+<!-- <script src="<?= assets('js/demo.js') ?>"></script> -->
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
